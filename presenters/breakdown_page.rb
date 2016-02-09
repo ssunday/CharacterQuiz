c10d6e19aaca0319f71788a42fb67a3ee88eb106
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-02-09 13:13:47 -0600

    Broke down some code.

--- presenters/breakdown_page.rb
+++ presenters/breakdown_page.rb
@@ -7,10 +7,20 @@ class BreakdownPage
   def show_percentage_breakdown
     percentage_breakdown = ""
     @scoring_breakdown.each do |character, percentage|
-      percentage_breakdown += "<h2> #{character}</h2>"
-      percentage_breakdown += "<h2> #{percentage.round}%<progress value='#{percentage}' max='100'></progress></h2>"
+      percentage_breakdown += character_name(character)
+      percentage_breakdown += percentage_bar(percentage)
     end
     percentage_breakdown
+  end
+
+  private
+
+  def character_name(character)
+    "<h2> #{character}</h2>"
+  end
+
+  def percentage_bar(percentage)
+    "<h2> #{percentage.round}%<progress value='#{percentage}' max='100'></progress></h2>"
   end
 
 end
--- presenters/results_page.rb
+++ presenters/results_page.rb
@@ -6,8 +6,18 @@ class ResultsPage
 
   def show_character_and_image
     character_and_image = ""
-    character_and_image += "<h1 style='text-align:center;'> Your Character is: #{@character}!</h1>"
-    character_and_image += "<p style='text-align:center;'><img src='images/#{@character}.png' alt='#{@character}' style='width:320px;height:510px;'></p>"
+    character_and_image += character_name_message
+    character_and_image += character_image
     character_and_image
+  end
+
+  private
+
+  def character_image
+    "<p style='text-align:center;'><img src='images/#{@character}.png' alt='#{@character}' style='width:320px;height:510px;'></p>"
+  end
+
+  def character_name_message
+    "<h1 style='text-align:center;'> Your Character is: #{@character}!</h1>"
   end
 end
